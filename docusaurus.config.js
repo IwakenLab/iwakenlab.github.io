a340bbdb065eecb07e634faef652deaa4b138dcc
iwaken71 <iwaken59371518@gmail.com>
2025-05-07 07:22:25 +0900

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,82 @@
+const config = {
+  title: 'Iwaken Lab.',
+  tagline: 'AIと検索エンジンが理解できるラボの公式サイト',
+  favicon: 'img/favicon.ico',
+  url: 'https://iwakenlab.github.io',
+  baseUrl: '/',
+  organizationName: 'iwakenlab',
+  projectName: 'iwakenlab-site',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/iwakenlab/iwakenlab-site/tree/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    navbar: {
+      title: 'Iwaken Lab.',
+      logo: {
+        alt: 'Iwaken Lab. Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'ドキュメント',
+        },
+        {
+          href: 'https://github.com/iwakenlab/iwakenlab-site',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'ドキュメント',
+          items: [
+            {
+              label: 'ラボ概要',
+              to: '/docs/index',
+            },
+            {
+              label: '理念・背景',
+              to: '/docs/philosophy',
+            },
+          ],
+        },
+        {
+          title: 'コミュニティ',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/iwakenlab',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Iwaken Lab. Built with Docusaurus.`,
+    },
+  },
+};
+
+module.exports = config; 
---
+++ sidebars.js
@@ -0,0 +1,30 @@
+const sidebars = {
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'ラボについて',
+      items: [
+        'index',
+        'philosophy',
+        'members_2025',
+      ],
+    },
+    {
+      type: 'category',
+      label: '活動',
+      items: [
+        'projects',
+        'history',
+      ],
+    },
+    {
+      type: 'category',
+      label: '参加する',
+      items: [
+        'faq',
+      ],
+    },
+  ],
+};
+
+module.exports = sidebars; 
